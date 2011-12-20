9ef97108b88e001543097f7becf195d16c874048
Joe Wandy <joewandy@gmail.com>
2011-12-20 02:50:15 +0000

    Completed mlclass-ex5 Regularized linear regression and bias-variance

--- linear-regression/mlclass-ex5/linearRegCostFunction.m
+++ linear-regression/mlclass-ex5/linearRegCostFunction.m
@@ -19,17 +19,22 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+% compute cost
+h_theta = X*theta;
+reg_theta = theta(2:end, :);
 
+reg_param = lambda/(2*m) * (reg_theta' * reg_theta);
+cost = (h_theta-y).^2;
+J = sum(1/(2*m) * cost) + reg_param;
 
+% compute the usual partial derivatives, without regularization
+pd = 1/m * (X' * (h_theta - y));
+pd_reg_param = (lambda/m)*theta;
 
-
-
-
-
-
-
-
-
+% apply regularization, except for theta = 1 so set first term = 0
+pd_reg_param(1) = 0;
+grad = pd + pd_reg_param;
+		
 % =========================================================================
 
 grad = grad(:);
